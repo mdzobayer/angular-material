8eb10b8d00f268ff9fc72f26fdc4f3aa9123c03e
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-09 15:54:21 +0600

    A fresh template for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,25 @@
-import { MatDialogModule } from '@angular/material/dialog';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import { MyDialogComponent } from './my-dialog/my-dialog.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MyDialogComponent
+    AppComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatDialogModule
+
   ],
   entryComponents: [
-    MyDialogComponent
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
